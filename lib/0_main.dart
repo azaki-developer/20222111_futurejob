cb577109b8f1caf1c315466b348f9a187b39f361
azaki-developer <azaki.developer@gmail.com>
2022-02-25 00:59:14 +0700

    added authprovider and userprovider

--- lib/0_main.dart
+++ lib/0_main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:futurejob/pages/1_splash_page.dart';
+import 'package:futurejob/providers/auth_provider.dart';
+import 'package:futurejob/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -8,9 +12,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) => AuthProvider()
+        ),
+        ChangeNotifierProvider<UserProvider>(
+          create: (context) => UserProvider()
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+      ),
     );
   }
 }
